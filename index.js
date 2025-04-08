4b9eb72273a61bd72b5cbbe83e0bde18e21c7ad0
arunmp1 <arunmp192000@gmail.com>
2025-04-08 12:34:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const pdf = require('html-pdf')
 require("dotenv").config();
 
 
-const upload = multer({
+ const upload = multer({
   storage: multer.memoryStorage(),
   limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
   fileFilter: (req, file, cb) => {
